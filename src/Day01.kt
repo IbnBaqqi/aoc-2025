2e1e36c9c209932acb2ee2138ef74891223919be
Abdulbaki <Salausmart132@gmail.com>
2025-12-02 09:27:02 +0200

    Day01-part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -18,13 +18,23 @@ fun main() {
     }
 
 
-    fun part2(input: List<String>): List<String> {
-        return input
+    fun part2(input: List<String>): Int {
+        var currPos = 50
+        var count = 0
+        input.forEach {
+            val move = if (it[0] == 'L') -1 else 1
+
+            repeat(it.drop(1).toInt()) {
+                currPos = (currPos + move) % 100
+                if (currPos == 0) count++
+            }
+        }
+        return count
     }
 
-    val inputTest = readInput("Day01")
-    part1(inputTest).println()
-//    part2(inputTest).println()
+    val input = readInput("Day01")
+    part1(input).println()
+    part2(input).println()
 }
 
 fun rotate(dir: Char, move: Int, currPos: Int) : Int {
